5c204cf81f8a9828cc15e138bc7210189b890ac5
lukecode <lucassocorrosilva@gmail.com>
2022-10-05 20:41:44 -0300

    Atualizado

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,50 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const terser = require("gulp-terser-js");
+const rename = require("gulp-rename");
+const webpack = require("webpack-stream");
 
 function css(done) {
   src("src/scss/app.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(postcss([autoprefixer()]))
-    .pipe(dest("build/css"));
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("public/build/css"));
 
   done();
 }
 
-function dev() {
+function js() {
+  return src("src/js/app.js")
+    .pipe(
+      webpack({
+        module: {
+          rules: [
+            {
+              test: /\.css$/i,
+              use: ["style-loader", "css-loader"],
+            },
+          ],
+        },
+        mode: "production",
+        watch: true,
+        entry: "./src/js/app.js",
+      })
+    )
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write("."))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest("./public/build/js"));
+}
+
+function dev(done) {
   watch("src/scss/**/*.scss", css);
-  watch;
+  watch("src/js/**/*.js", js);
+  done();
 }
 
 exports.css = css;
